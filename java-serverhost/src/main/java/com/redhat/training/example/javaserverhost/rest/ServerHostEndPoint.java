ac98bd07f98de54a85262c74a38b01bf5036199a
ste2003 <sandraencina@hotmail.com>
2021-04-24 22:46:39 -0300

    repair imports

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -4,6 +4,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
 import java.net.InetAddress;
 
 @Path("/")
